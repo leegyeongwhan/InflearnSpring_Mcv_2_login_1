ceae07d2e27a0372a7410072ecc2ab19304cf223
leegyeongwhan <leekhy02@naver.com>
2022-11-08 02:51:13 +0900

    feat(세션 정보와 타임아웃 설정): 세션의 타임 아웃 설정 정보 컨트롤러 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -60,7 +61,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV3(
             HttpServletRequest request, Model model
     ) {
@@ -71,6 +72,20 @@ public class HomeController {
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         //세션 관리자에 저장된 회원 정보 조회
+
+        //로그인
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model
+    ) {
 
         //로그인
         if (loginMember == null) {
