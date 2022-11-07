ca4b0a75a5e718ea6f9b815e0817552cedaf42f4
leegyeongwhan <leekhy02@naver.com>
2022-11-08 00:27:18 +0900

    feat(서블릿 세션 구현): 서블릿이 제공하는 HttpSession 구현

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -53,7 +55,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    // @PostMapping("/login")
     public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult
             , HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
@@ -72,16 +74,50 @@ public class LoginController {
 
         //세션관리자를 통해세션을 생성하고 회원데이터를 보관
         //쿠키에 시간정보를 주지않으면 세션쿠키가된다.(브라우저 종료시 종료
-        sessionManager.creatSession(loginMember, response);
 
 
         return "redirect:/";
     }
 
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult
+            , HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
 
-    @PostMapping("/logout")
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("loginMember={}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지않습니다");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공처리 TODO
+
+        HttpSession httpSession = request.getSession();
+        httpSession.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        //세션관리자를 통해세션을 생성하고 회원데이터를 보관
+        //쿠키에 시간정보를 주지않으면 세션쿠키가된다.(브라우저 종료시 종료
+
+
+        return "redirect:/";
+    }
+
+   // @PostMapping("/logout")
     public String logout(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
     }
 
